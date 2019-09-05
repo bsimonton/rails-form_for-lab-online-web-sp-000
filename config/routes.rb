96122dcbbcf4f4b8db6d267b9ed6bad6358377f3
Brian Simonton <bsimonton78@gmail.com>
2019-09-05 12:00:58 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :school_classes, only: [:index, :new, :create, :edit, :show, :update]
-  resources :students, only: 
+  resources :students, only: [:index, :new, :create, :edit, :show, :update]
   
 end
